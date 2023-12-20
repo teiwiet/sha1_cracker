d660549b6485848eac13b9e09b9261f0bffcdf4c
teiwiet <omegalmao1.3@gmail.com>
2023-12-20 14:02:34 +0700

    update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Simple sha1-cracker in rust 
 - :blush:
-- Usage : sha1_cracker <hash_2_crack> <word_list.txt>
+- Usage : sha1_cracker <word_list.txt> <hash_to_crack>
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use sha1::Digest;
 use std::{
     io,
     error::Error,
+    fs::File,
+    io::{BufRead,BufReader},
 };
 use std::env;
+use std::io::Read;
 
 
 const SHA1_HEX_STRING_LENGTH : usize = 40;
@@ -10,11 +14,24 @@ const SHA1_HEX_STRING_LENGTH : usize = 40;
 
 fn main()->Result<(),Box<dyn Error>>{
     let mut args : Vec<String> = env::args().collect();
-    if args.lent() !=3{
+    if args.len() !=3{
         println!("Usage : sha1_cracker <hash_2_crack> <word_list.txt>");
     }
-    if args[2].len() != SHA1_HEX_STRING_LENGTH{
+
+    let hash_2_crack  = &args[2];
+    if hash_2_crack.len() != SHA1_HEX_STRING_LENGTH{
         return Err("Hash is not valid".into());
+    }
+    //open file
+    let wordlist_file = File::open(&args[1])?;
+    let reader = BufReader::new(&wordlist_file);
+    for line in reader.lines(){
+        let line = line?;
+        let password = line.trim();
+        if hash_2_crack == &hex::encode(sha1::Sha1::digest(password.as_bytes())){
+            println!("Password found : {}",&password);
+            return Ok(());
+        }
     }
     Ok(())
 }
