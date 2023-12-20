13d5e127d368456a765c87ce4cd7472d087d0a07
teiwiet <omegalmao1.3@gmail.com>
2023-12-21 06:56:02 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,32 @@
-#![allow(unused)]
-use sha1::Digest;
-use std::fs::File;
-use hex;
 use std::{
     io,
-    io::{BufRead,BufReader},
+    fs::File,
     error::Error,
+    io::{BufRead,BufReader}
 };
+use sha1::Digest;
 use std::env;
-const HASH_LENGTH : usize = 40;
-fn main() -> Result<(),Box<dyn Error>> {
-    let args : Vec<String> = env::args().collect();
-    if args.len()!=3{
-        println!("Usage : sha1_cracker <wordlist> <hash_2_crack>");
-    }
 
-    let hash_2_crack = &args[2];
-    if hash_2_crack.len()!= HASH_LENGTH{
+const SHA1_STRING_LENGTH : usize = 40;
+fn main()->Result<(),Box<dyn Error>>{
+    let args:Vec<String> = env::args().collect();
+    if args.len() !=3{
+        println!("Usage : sha1_cracker <wordlist.txt> <hash_2_crack>");
+        return Ok(());
+    }
+    let hash_2_crack = args[2].trim();
+    if hash_2_crack.len() != SHA1_STRING_LENGTH{
         return Err("Invalid hash value".into());
     }
-    let mut lmao : Vec<String> = vec![];
     //open file
-    let file = File::open(args[1].trim())?;
+    let file = File::open(&args[1])?;
     let reader = BufReader::new(&file);
     for line in reader.lines(){
         let line = line?;
         let password = line.trim();
-        if hash_2_crack==&hex::encode(sha1::Sha1::digest(&password)){
-            println!("Found password : {}",password);
-            return Ok(());
+        if hash_2_crack == hex::encode(sha1::Sha1::digest(password.as_bytes())){
+            println!("Password found : {}",&password);
+            return Ok(())
         }
     }
     println!("Password not found");
